04ef598304604b4a38b1916832b0a71c755c08a1
BBarelli <breno.barelli4@gmail.com>
2025-06-03 18:15:10 -0300

    Power-Up

--- codigo.py
+++ codigo.py
@@ -1,4 +1,19 @@
-# Dica: Inicie deixandesclarecendo o que precisa ser feito.
+import pyautogui
+
+#pyautogui.click --> clique em algum lugar
+#pyautogui.press --> aperta 1 tecla
+#pyautogui.write --> escrever um texto
+
 '''
-PASSO 1: Entra no sistemas da empresa: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+Dica: Inicie deixandesclarecendo o que precisa ser feito.
+
+Passo 1: Entra no sistemas da empresa: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+Passo 2: Login
+Passo 3: Importar a base de dados
+Passo 4: Cadastrar 1 Produto 
+Passo 5: Repertir para todos os produtos
+
+
 '''
+
+# import pyautogui (permite controlar o mouse/teclado/tela do computador - usando python)
