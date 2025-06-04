4242ae5ceb0406ea17cebd15190bbb81492fd833
BBarelli <breno.barelli4@gmail.com>
2025-06-04 13:16:08 -0300

    Power-Up

--- codigo.py
+++ codigo.py
@@ -1,21 +1,8 @@
 import pyautogui
 import time
 
-'''
-# Dica: Inicie deixandesclarecendo o que precisa ser feito.
-# Entra no sistemas da empresa: https://dlp.hashtagtreinamentos.com/python/intensivao/login
-# import time  # Biblioteca para adiconar tempo na transição entre as telas (time.sleep(1))
 
- import pyautogui (permite controlar o mouse/teclado/tela do computador - usando python)
-
- pyautogui.click --> clique em algum lugar
- pyautogui.press --> aperta 1 tecla
- pyautogui.write --> escrever um texto
- pyautogui.hotkey --> apertar um combinação de teclas
- pyautogui.PAUSE --> definir uma configuração
-
-'''
-#Definir o tempo de transição entre as telas
+# Definir o tempo de transição entre as telas
 pyautogui.PAUSE = 1
 
 # Abri o chrome
@@ -28,10 +15,9 @@ pyautogui.press('enter')
 # Digitar o site
 pyautogui.write('https://dlp.hashtagtreinamentos.com/python/intensivao/login')
 pyautogui.press('enter')
-# Passo 2: Login
 time.sleep(2)
-
-
+# Passo 2: Login
+pyautogui.click()
 
 
 '''
