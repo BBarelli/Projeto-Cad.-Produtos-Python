7448933db8bad9b0a793a0ec378746de083bb139
BBarelli <breno.barelli4@gmail.com>
2025-06-04 13:16:35 -0300

    FAQ

---
+++ Faq.py
@@ -0,0 +1,16 @@
+# FAQ DO CÓDIGO:
+'''
+# Dica: Inicie deixandesclarecendo o que precisa ser feito.
+# Entra no sistemas da empresa: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# import time  # Biblioteca para adiconar tempo na transição entre as telas (time.sleep(1))
+
+ import pyautogui (permite controlar o mouse/teclado/tela do computador - usando python)
+
+ pyautogui.click --> clique em algum lugar
+ pyautogui.press --> aperta 1 tecla
+ pyautogui.write --> escrever um texto
+ pyautogui.hotkey --> apertar um combinação de teclas
+ pyautogui.PAUSE --> definir uma configuração
+ pyautogui.position
+
+'''
