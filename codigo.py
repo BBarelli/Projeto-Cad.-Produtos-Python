2a361113f705b8df2f57833a5e525f95e7402ed6
BBarelli <breno.barelli4@gmail.com>
2025-06-05 13:29:31 -0300

    Automação Python

--- codigo.py
+++ codigo.py
@@ -1,25 +1,3 @@
-# FAQ DO CÓDIGO:
-'''
-# Dica: Inicie deixandesclarecendo o que precisa ser feito.
-# Entra no sistemas da empresa: https://dlp.hashtagtreinamentos.com/python/intensivao/login
-# import time  # Biblioteca para adiconar tempo na transição entre as telas (time.sleep(1))
-
- import pyautogui (permite controlar o mouse/teclado/tela do computador - usando python)
-
- pyautogui.click --> clique em algum lugar
- pyautogui.press --> aperta 1 tecla
- pyautogui.write --> escrever um texto
- pyautogui.hotkey --> apertar um combinação de teclas
- pyautogui.PAUSE --> definir uma configuração
- pyautogui.click(button = rigth) // pyautogui.click(clicks = 2) dual clicks 
- pyautogui.scroll() número positivo: sobe // número negativo: desce
- print(pyautogui.position()) --> descobrir a posição do mouse em um campo dá um time.sleep(5) e...-->pyautogui.position(x=1883, y=624)
-
- Tratando ERRO:
- verificar o TypeErro
-
-'''
-
 import pyautogui
 import time
 import pandas as pd
